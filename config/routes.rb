8d29cd5434019afdb5ec7ece834f5fbef0f2ab0b
afil <urwillkome@gmail.com>
2018-04-27 23:03:55 +0800

    add bootstrap 4 gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/welcome'
   root 'pages#welcome'
+  resources :places
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
