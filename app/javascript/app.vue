4c31047bdc15fae757ded6009a699678853ab788
ToroChu <whileonrails@gmail.com>
2018-05-30 00:19:18 +0800

    Using Vue.js components in Rails views

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -5,12 +5,16 @@
 </template>
 
 <script>
+// export default {
+//   data: function () {
+//     return {
+//       message: "Hello Vue!"
+//     }
+//   }
+// }
+
 export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
+  props: ["message"],
 }
 </script>
 
--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -4,16 +4,28 @@
 // to the head of your layout file,
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
-import Vue from 'vue/dist/vue.esm'
+// import TurbolinksAdapter from 'vue-turbolinks';
+// import Vue from 'vue/dist/vue.esm'
+// import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  })
-})
+// Vue.use(TurbolinksAdapter)
+
+// Vue.component('app', App)
+
+// document.addEventListener('turbolinks:load', () => {
+//   const app = new Vue({
+//     el: '[data-behavior="vue"]',
+//   })
+// })
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   var app = new Vue({
+//     el: '#app',
+//     data: {
+//       message: 'Hello Vue!'
+//     }
+//   })
+// })
 
 // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
